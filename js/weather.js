55de90d04d980ff95698a130e29abd57cf8aa12c
chaeyubin <cyoobin@naver.com>
2023-07-21 14:40:40 +0900

    Feat: weather

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+const API_KEY = "4d9959bcdf978ae2a26e5aa0b480ff33";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  console.log("You live it", lat, lon);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
